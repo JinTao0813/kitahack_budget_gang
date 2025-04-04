aaf6142e1ea960cea48ec9ce0729edafb0bcf6f1
Wong Loo Perth <112810699+Perth00@users.noreply.github.com>
2025-04-04 23:43:55 +0800

    Update gemini_service.dart

--- lib/services/gemini_service.dart
+++ lib/services/gemini_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class GeminiService {
-  final String apiKey = 'AIzaSyDbxHapbOMNXydDyQB85aVvOFj0P9_NVeg';
+  final String apiKey = 'api key here ';
 
   Future<String> getASLInfo(List<int>? numbers) async {
     final prompt =
